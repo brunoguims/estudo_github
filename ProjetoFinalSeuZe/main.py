f9b8b1589c4519c13122af64dfa4700f6122a39f
Bruno Guimaraes <guimaraesbruno1405@gmail.comgit config listgit config --global user.email guimaraesbruno1405@gmail.com>
2024-12-01 20:28:16 -0300

    alteracoes_projeto

--- ProjetoFinalSeuZe/main.py
+++ ProjetoFinalSeuZe/main.py
@@ -18,13 +18,33 @@ try:
         advice = result.json()['slip']['advice']
 
         traducao = GoogleTranslator(source='english', target='portuguese').translate(advice)
-        logger.success('Tradução realizada com sucesso.')
 
-        with open('arquivo.txt', 'a', encoding='UTF-8') as arquivo:
-            arquivo.write(f'{id} - {advice} - {traducao} \n')
-        logger.success('Registro no banco de dados(txt) realizado com sucesso.')
+        print(f'Conselho:{id} {advice} - Tradução: {traducao}.')
+        print('Deseja Salvar ou Excluir? \nTecle (S) para salvar ou tecle (X) para excluir.')
 
+        sx_conselho = input('Digite aqui o que deseja: ').upper()
+
+
+
+        if sx_conselho == 'S':
+            with open('arquivo.txt', 'a', encoding='UTF-8') as arquivo:
+                arquivo.write(f'{id} - {advice} - {traducao} \n')
+            logger.success('Registro no banco de dados(txt) realizado com sucesso.')
+        else:
+            logger.info('Conselho Excluido')
         sleep(1)
+
+        print('Gostaria de ver todos os conselhos presente no banco de dados?')
+        ver_conselhos = input('Digite (V) para ver e (N) para encerrar o programa: ').upper()
+
+        if ver_conselhos == 'V':
+            with open('arquivo.txt', 'r', encoding='UTF-8') as arquivo:
+                arquivos_banco = arquivo.read()
+            print(arquivos_banco)
+            logger.info('Programa Encerrado')
+
+        else:
+            logger.info('Programa Encerrado')
 
         logger.info(f'Fim do ciclo {i + 1}.')
 except Exception as e:
